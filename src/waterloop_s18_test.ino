66f7719f6e9893b191d8b96a66f11613e7834cf3
Haaris710 <31973970+Haaris710@users.noreply.github.com>
2018-05-12 14:06:14 -0400

    Add files via upload

---
+++ src/waterloop_s18_test.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+
+uint8_t POD_ADDRESS = 0x5A;
+
+#define STX = 0x56;
+#define ETX = 0x32;
+
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+}
+
+
